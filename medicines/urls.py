1d29a388f2bd3d40cc03e54f19374799927ce051
spicylemonade15 <acads108@gmail.com>
2025-09-30 12:00:08 +0530

    add auth and requirements

--- medicines/urls.py
+++ medicines/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Auth
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    # CRUD
     path('', views.medication_list, name='med_list'),
     path('add/', views.medication_create, name='med_add'),
     path('edit/<int:pk>/', views.medication_update, name='med_edit'),
